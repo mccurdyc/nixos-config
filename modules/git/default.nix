f7f072ddec2656176d4eff04a2473c75cb00a992
Colton J. McCurdy <mccurdyc22@gmail.com>
2022-09-13 20:30:53 -0400

    git: aliases
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -23,6 +23,14 @@ in {
           excludesfile = "$NIXOS_CONFIG_DIR/scripts/gitignore";
         };
       };
+      aliases = {
+        l = "log --graph --topo-order --abbrev-commit --date=short --decorate --all --boundary";
+        ll = "log --graph --all  --decorate --oneline";
+        ls = "log --pretty=format:\"%C(green)%h %C(yellow)[%ad]%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=relative";
+        ld = "log --all --graph --abbrev-commit --decorate --pretty=format:\"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset) %C(dim white)- %an%C(reset)\"";
+        d = "difftool";
+        m = "mergetool";
+      };
     };
 
     programs.gitui = {
