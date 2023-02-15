fbbdc7a1cd0bdfd61526691293a44544dd5bed03
Colton J. McCurdy <mccurdyc22@gmail.com>
2023-02-14 21:49:26 -0500

    add go path to PATH
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -11,5 +11,8 @@ in
   config = mkIf cfg.enable {
     # force it to create the directory
     home.file."src/github.com/mccurdyc".text = "";
+    home.sessionPath = [
+      "$HOME/go/bin"
+    ];
   };
 }
