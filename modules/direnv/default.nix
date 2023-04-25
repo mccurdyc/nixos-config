ab6da183dec666c5efeba4fd44716a644e3bf1eb
Colton J. McCurdy <mccurdyc22@gmail.com>
2023-04-25 14:57:42 -0400

    adds direnv
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- modules/direnv/default.nix
+++ modules/direnv/default.nix
@@ -12,7 +12,5 @@ in
     # https://github.com/nix-community/nix-direnv#via-home-manager
     programs.direnv.enable = true;
     programs.direnv.nix-direnv.enable = true;
-    # optional for nix flakes support in home-manager 21.11, not required in home-manager unstable or 22.05
-    programs.direnv.nix-direnv.enableFlakes = true;
   };
 }
