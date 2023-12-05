74bb1528ec8e90bf38ed0ad60d89063bf0d80c23
Colton J. McCurdy <mccurdyc22@gmail.com>
2023-12-05 10:15:05 -0500

    fix CTRL-P not working
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/nvim/config/init.lua
+++ home-modules/shared/nvim/config/init.lua
@@ -357,7 +357,6 @@ require("lazy").setup({
     }, {
         "nvim-telescope/telescope.nvim",
         dependencies = {"nvim-lua/plenary.nvim"},
-        version = "0.1.1",
         config = function()
             require("telescope").setup {
                 defaults = {
