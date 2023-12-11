ef317cab172b02bc2c366f2dc115f3830b3f1920
Colton J. McCurdy <mccurdyc22@gmail.com>
2023-12-11 15:25:42 -0500

    adds zellij config
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/zellij.nix
+++ home-modules/shared/zellij.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 
 {
   programs.zellij.enable = true;
+  programs.zellij.package = pkgs-unstable.zellij;
   programs.zellij.enableZshIntegration = true;
 
   home.file.".config/zellij/config.kdl".source = ./zellij/config.kdl;
