df8a5c97b4e62d01b2a6afbaf9429c0715062e3d
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-02-16 08:33:47 -0500

    [nvim] - neotree fix key mappings that I always stumbled on
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/nvim/config/init.lua
+++ home-modules/shared/nvim/config/init.lua
@@ -147,29 +147,148 @@ require("lazy").setup({
         event = {"CmdlineEnter"},
         ft = {"go", 'gomod'},
         build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-    }, {"rust-lang/rust.vim", ft = "rs"}, "nvim-tree/nvim-web-devicons", {
-        "nvim-neo-tree/neo-tree.nvim",
-        {
+    }, { 
+	"rust-lang/rust.vim", ft = "rs"
+    }, {
             "nvim-neo-tree/neo-tree.nvim",
             branch = "v3.x",
             dependencies = {
-                "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
                 "MunifTanjim/nui.nvim"
-            }
-        },
-        "sindrets/diffview.nvim",
-        config = function()
-            local g = vim.g
+            },
+            config = function ()
+                -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+                vim.fn.sign_define("DiagnosticSignError",
+                  {text = " ", texthl = "DiagnosticSignError"})
+                vim.fn.sign_define("DiagnosticSignWarn",
+                  {text = " ", texthl = "DiagnosticSignWarn"})
+                vim.fn.sign_define("DiagnosticSignInfo",
+                  {text = " ", texthl = "DiagnosticSignInfo"})
+                vim.fn.sign_define("DiagnosticSignHint",
+                  {text = "󰌵", texthl = "DiagnosticSignHint"})
 
-            -- Open NvimTree on Vim open.
-            -- vim.cmd [[autocmd VimEnter * NvimTreeOpen]]
-            --
-
-            require("neo-tree").setup({
-                source_selector = {winbar = false, statusline = false}
-            })
-        end
-    }, {
+                require("neo-tree").setup({
+                    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+                    popup_border_style = "rounded",
+                    enable_git_status = true,
+                    source_selector = {winbar = false, statusline = false},
+                    enable_diagnostics = true,
+                    icon = {
+                        folder_closed = "",
+                        folder_open = "",
+                        folder_empty = "󰜌",
+                        -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+                        -- then these will never be used.
+                        default = "*",
+                        highlight = "NeoTreeFileIcon"
+                    },
+                    modified = {
+                        symbol = "[+]",
+                        highlight = "NeoTreeModified",
+                    },
+                    name = {
+                        trailing_slash = false,
+                        use_git_status_colors = true,
+                        highlight = "NeoTreeFileName",
+                    },
+                    git_status = {
+                        symbols = {
+                            -- Change type
+                            added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                            modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                            deleted   = "✖",-- this can only be used in the git_status source
+                            renamed   = "󰁕",-- this can only be used in the git_status source
+                            -- Status type
+                            untracked = "",
+                            ignored   = "",
+                            unstaged  = "󰄱",
+                            staged    = "",
+                            conflict  = "",
+                        }
+                    },
+                    -- A list of functions, each representing a global custom command
+                    -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
+                    -- see `:h neo-tree-custom-commands-global`
+                    commands = {},
+                    window = {
+                        position = "left",
+                        width = 40,
+                        mapping_options = {
+                            noremap = true,
+                            nowait = true,
+                        },
+                        mappings = {
+                            ["<cr>"] = "toggle_node",
+                            ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+                            ["x"] = "open_split", -- split_with_window_picker
+                            ["v"] = "open_vsplit", -- vsplit_with_window_picker
+                            ["w"] = "open_with_window_picker",
+                            ["bs"] = "close_node", -- close_all_subnodes
+                            ["zM"] = "close_all_nodes",
+                            ["O"] = "expand_all_nodes",
+                            ["a"] = { 
+                                "add",
+                                -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+                                -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                                config = {
+                                  show_path = "none" -- "none", "relative", "absolute"
+                                }
+                            },
+                            ["A"] = "add_directory",
+                            ["d"] = "delete",
+                            ["r"] = "rename",
+                            ["y"] = "copy_to_clipboard",
+                            ["p"] = "paste_from_clipboard",
+                            ["r"] = "rename",
+                            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+                            ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                            ["q"] = "close_window",
+                            ["R"] = "refresh",
+                            ["?"] = "show_help",
+                            ["<"] = "navigate_up",
+                            [">"] = "navigate_down",
+                            ["."] = "set_root",
+                            ["H"] = "toggle_hidden",
+                            ["/"] = "fuzzy_finder",
+                            ["#"] = "fuzzy_sorter",
+			    ["og"] = { "order_by_git_status", nowait = false },
+			    ["on"] = { "order_by_name", nowait = false },
+                            ["i"] = "show_file_details",
+                        },
+			fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+			      ["<C-n>"] = "move_cursor_down",
+			      ["<C-p>"] = "move_cursor_up",
+			},
+                    },
+	            filesystem = {
+			  filtered_items = {
+			    visible = false, -- when true, they will just be displayed differently than normal items
+			    hide_dotfiles = true,
+			    hide_gitignored = true,
+			    hide_hidden = true, -- only works on Windows for hidden files/directories
+			    hide_by_name = {
+			      --"node_modules"
+			    },
+			    hide_by_pattern = { -- uses glob style patterns
+			      --"*.meta",
+			      --"*/src/*/tsconfig.json",
+			    },
+			    always_show = { -- remains visible even if other settings would normally hide it
+			      --".gitignored",
+			    },
+			    never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+			      --".DS_Store",
+			      --"thumbs.db"
+			    },
+			    never_show_by_pattern = { -- uses glob style patterns
+			      --".null-ls_*",
+			    }
+			  }
+		    }
+                  })
+		end
+	}, {
         "hrsh7th/nvim-cmp",
         -- load cmp on InsertEnter
         event = "InsertEnter",
