65809b1d86d0f5846449f4b43e34754113673c11
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-03-13 09:29:25 -0400

    [git] aliase lb - list commits on current branch
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/git.nix
+++ home-modules/shared/git.nix
@@ -42,7 +42,8 @@
     };
     aliases = {
       l = "log --graph --topo-order --abbrev-commit --date=short --decorate --all --boundary";
-      ll = "log --graph --all  --decorate --oneline";
+      lb = "!f() { git log --graph $(git branch --show-current) --not origin/main --decorate --oneline; }; f";
+      ll = "log --graph --all --decorate --oneline";
       ls = "log --pretty=format:\"%C(green)%h %C(yellow)[%ad]%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=relative";
       ld = "log --all --graph --abbrev-commit --decorate --pretty=format:\"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset) %C(dim white)- %an%C(reset)\"";
       d = "difftool";
