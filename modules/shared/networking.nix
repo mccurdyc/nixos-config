0dccb255fe9f09d09de625a0661c59e6aac23218
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-06-11 07:43:08 -0400

    use tailscale-recommended resolved instead of resolve-conf
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- modules/shared/networking.nix
+++ modules/shared/networking.nix
@@ -5,4 +5,13 @@
     enable = true;
     package = pkgs-unstable.tailscale;
   };
+
+  # Why resolved?
+  # It's the recommendation from Tailscale - https://tailscale.com/kb/1235/resolv-conf#how-do-i-stop-tailscaled-from-overwriting-etcresolvconf
+  services.resolved = {
+    enable = true;
+    fallbackDns = [
+      "100.100.100.100"
+    ];
+  };
 }
