743c8eb9cb2abf8ec326477b95d033ed6398e064
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-07-19 09:35:23 -0400

    [ssh] serverAlive settings
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/ssh.nix
+++ home-modules/shared/ssh.nix
@@ -5,6 +5,8 @@ _: {
     controlPath = "~/.ssh/master-%r@%n:%p";
     controlPersist = "30m";
     forwardAgent = true;
+    serverAliveInterval = 30;
+    serverAliveCountMax = 120;
 
     matchBlocks = {
       "*" = { };
