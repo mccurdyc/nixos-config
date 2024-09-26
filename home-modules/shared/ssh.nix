04a1b9c0698acfc82f32d1334d1828f05bcda733
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-09-26 09:19:33 -0400

    work ssh jetpac
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/ssh.nix
+++ home-modules/shared/ssh.nix
@@ -1,4 +1,5 @@
 _: {
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.ssh.matchBlocks
   programs.ssh = {
     enable = true;
     controlMaster = "no";
@@ -7,6 +8,16 @@ _: {
     forwardAgent = true;
     serverAliveInterval = 30;
     serverAliveCountMax = 120;
+
+    # Today, I just manage these ssh configs outside of Nix because I don't currently
+    # have a way to "inject" or "consume" secret Nix modules.
+    # https://man7.org/linux/man-pages/man5/ssh_config.5.html
+    #
+    # eval $(op signin) && op document get <name> --output $HOME/.ssh/main
+    includes = [
+      "~/.ssh/config.d/work/main.conf"
+      "~/.ssh/config.d/work/jetpac.conf"
+    ];
 
     matchBlocks = {
       "*" = { };
--- hosts/faamac/packages.nix
+++ hosts/faamac/packages.nix
@@ -3,5 +3,6 @@
 {
   environment.systemPackages = with pkgs; [
     wireguard-tools
+    google-cloud-sdk
   ];
 }
