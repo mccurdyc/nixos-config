ed9fe11c8272532d79d44e174060eedc4607593b
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-11-11 07:10:47 -0500

    hosts networking for mosh
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- hosts/nuc/networking.nix
+++ hosts/nuc/networking.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ config, ... }:
 
 {
   networking.hostName = "nuc";
+
+  programs.mosh.enable = true;
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 ];
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+    allowedUDPPortRanges = [{ from = 60000; to = 61000; }];
+    checkReversePath = "loose";
+  };
 }
