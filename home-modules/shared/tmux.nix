3f7689e23968f194fe2d569662f84bf0bf071992
Colton J. McCurdy <mccurdyc22@gmail.com>
2025-07-26 05:27:31 -0400

    color refactor 0o0
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/tmux.nix
+++ home-modules/shared/tmux.nix
@@ -1,23 +1,5 @@
 { pkgs, ... }: {
 
-  # 00 - #ff5f5f red (IndianRed1) (error) ctermfg=203
-  # 01 - #ffa500 orange (Orange1) (warning) ctermfg=214
-  # 02 - #5fd787 green (SeaGreen3) (good) ctermfg=78
-  # 03 - #2950c5 blue (alt) no direct ctermfg, use ctermfg=33
-  # 04 - empty (alt) ->
-  # 05 - empty (alt) ->
-  # 06 - empty (alt) ->
-  # --- Black and White
-  # 07 - #000000 pure black (Black) ctermfg=00
-  # 08 - #1c1c1c "black" (Grey11) (background) ctermfg=234
-  # 09 - #262626 really dark gray (Grey15) (comment) ctermfg=235
-  # 0A - #4e4e4e dark grey (Grey30) (more important than comment) ctermfg=29
-  # 0B - #9e9e9e (247_Grey62) ctermfg=247
-  # 0C - #d3d0c8 (252_Grey82) ctermfg=252
-  # 0D - #e4e4e4 light grey (Grey89) (foreground) ctermfg=254
-  # 0E - #eeeeee "white" (Grey93) (foreground) ctermfg=255
-  # 0F - #ffffff pure white (White) ctermfg=16
-
   programs.tmux = {
     enable = true;
     baseIndex = 0;
@@ -52,11 +34,11 @@
       set-option -g status-left-length 8
       set-option -g status-right-length 20
 
-      set -g window-status-format " • #[fg=#ffa500,bg=#1c1c1c]#W"
-      set -g window-status-current-format " • #[fg=#ffa500,bg=#1c1c1c]#W"
+      set -g window-status-format " • #[fg=#ffa500,bg=#040405]#W"
+      set -g window-status-current-format " • #[fg=#ffa500,bg=#040405]#W"
 
-      set-option -g status-left "#[fg=#1c1c1c,bg=#ffa500,bold]#S"
-      set-option -g status-right "#[fg=#1c1c1c,bg=#ffa500,bold]#(date -u "+%%H:%%M") • #[fg=#1c1c1c,bg=#ffa500,bold]#(uptime | cut -d ' ' -f 2)"
+      set-option -g status-left "#[fg=#040405,bg=#ffa500,bold]#S"
+      set-option -g status-right "#[fg=#040405,bg=#ffa500,bold]#(date -u "+%%H:%%M") • #[fg=#040405,bg=#ffa500,bold]#(uptime | cut -d ' ' -f 2)"
 
       # copy-paste Mac -> Terminal Emulator -> ssh -> tmux -> neovim
       # Cmd+c (copy), Cmd+v (paste)
@@ -84,20 +66,21 @@
 
       # default statusbar colors
       set-option -g status-interval 1
-      set-option -g status-style "fg=#ffa500,bg=#1c1c1c"
+      set-option -g status-style "fg=#ffa500,bg=#040405"
 
       # default window title colors
-      set-window-option -g window-status-style "fg=#ffa500,bg=#1c1c1c"
+      set-window-option -g window-status-style "fg=#ffa500,bg=#040405"
+      set-window-option -g window-style "fg=#ffa500,bg=#040405"
 
       # active window title colors
-      set-window-option -g window-status-current-style "fg=#ffa500,bg=#1c1c1c"
+      set-window-option -g window-status-current-style "fg=#ffa500,bg=#040405"
 
       # pane border
-      set-option -g pane-border-style "fg=#1c1c1c"
+      set-option -g pane-border-style "fg=#040405"
       set-option -g pane-active-border-style "fg=#ffa500"
 
       # message text
-      set-option -g message-style "fg=#ffa500,bg=#1c1c1c"
+      set-option -g message-style "fg=#ffa500,bg=#040405"
 
       # pane number display
       set-option -g display-panes-active-colour "#ffa500"
@@ -107,10 +90,10 @@
       set-window-option -g clock-mode-colour "#ffa500"
 
       # copy mode highlight
-      set-window-option -g mode-style "bg=#4e4e4e"
+      set-window-option -g mode-style "bg=#2d2d2d"
 
       # bell
-      set-window-option -g window-status-bell-style "fg=#1c1c1c,bg=#ffa500"
+      set-window-option -g window-status-bell-style "fg=#040405,bg=#ffa500"
 
       set -g @plugin 'tmux-plugins/tpm'
       set -g @plugin 'tmux-plugins/tmux-yank'
--- home-modules/shared/zsh.nix
+++ home-modules/shared/zsh.nix
@@ -1,23 +1,5 @@
 { pkgs, lib, ... }: {
 
-  # 00 - #ff5f5f red (IndianRed1) (error) ctermfg=203
-  # 01 - #ffa500 orange (Orange1) (warning) ctermfg=214
-  # 02 - #5fd787 green (SeaGreen3) (good) ctermfg=78
-  # 03 - #2950c5 blue (alt) no direct ctermfg, use ctermfg=33
-  # 04 - empty (alt) ->
-  # 05 - empty (alt) ->
-  # 06 - empty (alt) ->
-  # --- Black and White
-  # 07 - #000000 pure black (Black) ctermfg=00
-  # 08 - #1c1c1c "black" (Grey11) (background) ctermfg=234
-  # 09 - #262626 really dark gray (Grey15) (comment) ctermfg=235
-  # 0A - #4e4e4e dark grey (Grey30) (more important than comment) ctermfg=29
-  # 0B - #9e9e9e (247_Grey62) ctermfg=247
-  # 0C - #d3d0c8 (252_Grey82) ctermfg=252
-  # 0D - #e4e4e4 light grey (Grey89) (foreground) ctermfg=254
-  # 0E - #eeeeee "white" (Grey93) (foreground) ctermfg=255
-  # 0F - #ffffff pure white (White) ctermfg=16
-
   programs.zsh = {
     enable = true;
     defaultKeymap = "viins";
@@ -89,7 +71,7 @@
       export ZELLIJ_AUTO_EXIT=true
       export CTAGS_COMMAND=ctags # zoekt - https://github.com/sourcegraph/zoekt/blob/5ac92b1a7d4ab7b0dbeeaa9df77abb13d555e16b/doc/ctags.md?plain=1#L18-L21
       # https://minsw.github.io/fzf-color-picker/
-      export FZF_DEFAULT_OPTS='--color=fg:#9e9e9e,bg:#1c1c1c,hl: --color=fg+:#e4e4e4,bg+:#262626,hl+:#ffa500 --color=info:#9e9e9e,prompt:#ffa500,pointer:#ffa500 --color=marker:#ff5f5f,spinner:#ffa500,header:#ffa500'
+      export FZF_DEFAULT_OPTS='--color=fg:#9e9e9e,bg:#040405,hl: --color=fg+:#e4e4e4,bg+:#262626,hl+:#ffa500 --color=info:#9e9e9e,prompt:#ffa500,pointer:#ffa500 --color=marker:#ff5f5f,spinner:#ffa500,header:#ffa500'
     '';
 
     history = {
