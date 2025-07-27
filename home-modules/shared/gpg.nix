0af4b2545c1aa98dd44f5dfd6b1e8f208cbbbdf9
Colton J. McCurdy <mccurdyc22@gmail.com>
2025-07-27 19:35:56 -0400

    visual select color
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/gpg.nix
+++ home-modules/shared/gpg.nix
@@ -3,10 +3,19 @@
 {
   programs.gpg = {
     enable = true;
+    settings = {
+      use-agent = true;
+      pinentry-mode = "loopback"; # Optional: force loopback mode
+    };
   };
 
   services.gpg-agent = {
     enable = true;
     pinentry.package = pkgs.pinentry-curses;
+    defaultCacheTtl = 28800; # 8 hours
+    maxCacheTtl = 86400; # 24 hours
+    extraConfig = ''
+      allow-loopback-pinentry
+    '';
   };
 }
--- home-modules/shared/zsh.nix
+++ home-modules/shared/zsh.nix
@@ -40,263 +40,268 @@
       whatsmyip = "dig +short myip.opendns.com @resolver1.opendns.com";
       curls = ''curl -o /dev/null -s -w "%{http_code}\n"'';
       ghpr = "(){ gh pr create --fill --draft $@ ;}";
-      g = ''nvim -c "Neogit" -c "autocmd WinClosed * if winnr('$') == 1 | qall | endif"'';
+      g = ''nvim -c 'Neogit' -c 'autocmd WinClosed,BufEnter * if winnr("$") == 1 && empty(expand("%")) && !&modified && getline(1, "$") == [""] | qall | endif''';
       gits = "git status";
-      gitfc = ''(){ git log --format=format:"%H" | tail -1 ;}'';
-      kubectl_pods_containers = ''kubectl get pods -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{": \t "}{range .spec.containers[*]}{.name}{", "}{end}{end}' | sort'';
-      docker_ps = ''docker ps --format "{{.Names}}\t{{.Ports}}\t{{.Status}}"'';
-      k = "kubectl";
-      zkt = "zoekt -index_dir $HOME/zoekt-serving/index -r";
-      zobs = "zoekt -index_dir $HOME/zoekt-serving/index \"r:obsidian.md $@\"";
-      stopwatch = ''
-        start=$(date +%s)
-        if [[ $1 == ?([+-])+([0-9]) ]]; then
-         ((start += $1))
-        elif [[ $1 ]]; then
-         echo "invalid argument '$1': ignoring it"
+      gitfc = ''
+        (){ git log --format = format: "%H" | tail - 1; } '';
+      kubectl_pods_containers = ''
+          kubectl
+          get
+          pods - o jsonpath='{
+        range .items[*]}{"\n"}{.metadata.name}{": \t "}{range .spec.containers[*]}{.name}{", "}{end}{end}' | sort'';
+        docker_ps = ''docker ps --format "{{.Names}}\t{{.Ports}}\t{{.Status}}"'';
+        k = "kubectl";
+        zkt = "zoekt -index_dir $HOME/zoekt-serving/index -r";
+        zobs = "zoekt -index_dir $HOME/zoekt-serving/index \"r:obsidian.md $@\"";
+        stopwatch = ''
+          start=$(date +%s)
+          if [[ $1 == ?([+-])+([0-9]) ]]; then
+           ((start += $1))
+          elif [[ $1 ]]; then
+           echo "invalid argument '$1': ignoring it"
+          fi
+
+          while true; do
+           now=$(date +%s)
+           days=$(( (now - start) / 86400 ))
+           seconds=$(( (now - start) % 86400 ))
+           printf "\r%d day(s) and %s " $days $(date --utc --date @$seconds +%T)
+           sleep 0.1
+          done
+        '';
+      };
+
+      envExtra = ''
+        export ZELLIJ_AUTO_ATTACH=false
+        export ZELLIJ_AUTO_EXIT=true
+        export CTAGS_COMMAND=ctags # zoekt - https://github.com/sourcegraph/zoekt/blob/5ac92b1a7d4ab7b0dbeeaa9df77abb13d555e16b/doc/ctags.md?plain=1#L18-L21
+        # https://minsw.github.io/fzf-color-picker/
+        export FZF_DEFAULT_OPTS='--color=fg:#9e9e9e,bg:#040405,hl: --color=fg+:#e4e4e4,bg+:#262626,hl+:#ffa500 --color=info:#9e9e9e,prompt:#ffa500,pointer:#ffa500 --color=marker:#ff5f5f,spinner:#ffa500,header:#ffa500'
+      '';
+
+      history = {
+        size = 2000;
+        save = 2000;
+        ignoreDups = true;
+        ignoreSpace = true;
+        # - https://zsh.sourceforge.io/Doc/Release/Options.html#History
+        # 'share' basically enables 'extended' automatically.
+        share = true;
+        extended = true;
+        # https://askubuntu.com/questions/999923/syntax-in-history-ignore
+        ignorePatterns = [
+          ":w"
+          ":wq"
+          "cd#( *)#"
+          "cp#( *)#*"
+          "fg"
+          "git init#( *)#*"
+          "git add#( *)#*"
+          "git checkout#( *)#*"
+          "git clone#( *)#*"
+          "git clean#( *)#*"
+          "git commit#( *)#*"
+          "git diff#( *)#*"
+          "git ll"
+          "git log"
+          "git pull#( *)#*"
+          "git push#( *)#"
+          "git show#( *)#"
+          "gits"
+          "kill#( *)#"
+          "l[sa]#( *)#*"
+          "mv #( *)#"
+          "nvim#( *)#*"
+          "mkdir#( *)#*"
+          "which#( *)#*"
+          "touch#( *)#*"
+          "pkill#( *)#"
+          "rm#( *)#"
+          "tmux"
+          "tl"
+          "z#( *)#"
+        ];
+      };
+      profileExtra = "";
+      dotDir = ".config/zsh";
+      loginExtra = ''
+        # This has to come after where FZF_DEFAULT_OPTS is set
+        # Remember that fzf-tab doesn't follow FZF_DEFAULT_OPTS by default. If you want it to use your default fzf options, you can set:
+        zstyle ':fzf-tab:*' use-fzf-default-opts yes
+      '';
+      initContent = lib.mkOrder 550 ''
+        zstyle ':completion:*' menu select
+
+        # https://vninja.net/2024/12/28/ghostty-workaround-for-missing-or-unsuitable-terminal-xterm-ghostty/
+        if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
+          export TERM=xterm-256color
         fi
 
-        while true; do
-         now=$(date +%s)
-         days=$(( (now - start) / 86400 ))
-         seconds=$(( (now - start) % 86400 ))
-         printf "\r%d day(s) and %s " $days $(date --utc --date @$seconds +%T)
-         sleep 0.1
-        done
+        setopt clobber
+        setopt extendedglob
+        setopt interactive_comments
+        setopt nobeep
+        setopt HIST_IGNORE_ALL_DUPS
+        setopt HIST_REDUCE_BLANKS
+
+        # NOTE: I had issues where fzf-tab wasn't including hidden files even
+        # though my FZF_DEFAULT_COMMAND specifies to do so.
+        # https://github.com/Aloxaf/fzf-tab/issues/193#issuecomment-784722265
+        setopt globdots
+
+        eval "$(starship init zsh)"
+        eval "$(just --completions zsh)"
+
+        source ${pkgs.fzf}/share/fzf/completion.zsh
+        # NOTE: I had issues with zsh-vi-mode overwriting ^R
+        source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+
+        source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+        source ${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh
+
+        if [ -x "$(command -v stern)" ]; then
+          source <(stern --completion=zsh)
+        fi
+
+        if [ -x "$(command -v fastly)" ]; then
+          eval "$(fastly --completion-script-zsh)"
+        fi
+
+        if [ -x "$(command -v dagger)" ]; then
+          source <(dagger completion zsh)
+        fi
+
+        if [ -x "$(command -v k3d)" ]; then
+          source <(k3d completion zsh)
+        fi
+
+        eval $(keychain --eval --quiet ~/.ssh/config.d/work/fastly_rsa)
+
+        # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-completion.html#cli-command-completion-path
+        autoload bashcompinit && bashcompinit
+        autoload -Uz compinit && compinit
+
+        complete -C "${pkgs.awscli2}/bin/aws_completer" aws
+
+        eval "$(zoxide init zsh)"
       '';
     };
 
-    envExtra = ''
-      export ZELLIJ_AUTO_ATTACH=false
-      export ZELLIJ_AUTO_EXIT=true
-      export CTAGS_COMMAND=ctags # zoekt - https://github.com/sourcegraph/zoekt/blob/5ac92b1a7d4ab7b0dbeeaa9df77abb13d555e16b/doc/ctags.md?plain=1#L18-L21
-      # https://minsw.github.io/fzf-color-picker/
-      export FZF_DEFAULT_OPTS='--color=fg:#9e9e9e,bg:#040405,hl: --color=fg+:#e4e4e4,bg+:#262626,hl+:#ffa500 --color=info:#9e9e9e,prompt:#ffa500,pointer:#ffa500 --color=marker:#ff5f5f,spinner:#ffa500,header:#ffa500'
-    '';
+    xdg.configFile."zsh/.zsh_history.default".source = ./zsh/.zsh_history.default;
 
-    history = {
-      size = 2000;
-      save = 2000;
-      ignoreDups = true;
-      ignoreSpace = true;
-      # - https://zsh.sourceforge.io/Doc/Release/Options.html#History
-      # 'share' basically enables 'extended' automatically.
-      share = true;
-      extended = true;
-      # https://askubuntu.com/questions/999923/syntax-in-history-ignore
-      ignorePatterns = [
-        ":w"
-        ":wq"
-        "cd#( *)#"
-        "cp#( *)#*"
-        "fg"
-        "git init#( *)#*"
-        "git add#( *)#*"
-        "git checkout#( *)#*"
-        "git clone#( *)#*"
-        "git clean#( *)#*"
-        "git commit#( *)#*"
-        "git diff#( *)#*"
-        "git ll"
-        "git log"
-        "git pull#( *)#*"
-        "git push#( *)#"
-        "git show#( *)#"
-        "gits"
-        "kill#( *)#"
-        "l[sa]#( *)#*"
-        "mv #( *)#"
-        "nvim#( *)#*"
-        "mkdir#( *)#*"
-        "which#( *)#*"
-        "touch#( *)#*"
-        "pkill#( *)#"
-        "rm#( *)#"
-        "tmux"
-        "tl"
-        "z#( *)#"
-      ];
-    };
-    profileExtra = "";
-    dotDir = ".config/zsh";
-    loginExtra = ''
-      # This has to come after where FZF_DEFAULT_OPTS is set
-      # Remember that fzf-tab doesn't follow FZF_DEFAULT_OPTS by default. If you want it to use your default fzf options, you can set:
-      zstyle ':fzf-tab:*' use-fzf-default-opts yes
-    '';
-    initContent = lib.mkOrder 550 ''
-      zstyle ':completion:*' menu select
+    programs.starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        package.disabled = true;
+        format = "\\\[$username[@](bold #5fd787)$hostname\\\] \\\[$directory\\\] (\\\[($git_state )($git_status)($git_branch)($git_commit)\\\])($sudo)($golang)($helm)($lua)($nodejs)($ruby)($rust)($terraform)( \\\[$jobs\\\])( \\\[$status\\\])( \\\[$shell\\\])$line_break$time $character";
 
-      # https://vninja.net/2024/12/28/ghostty-workaround-for-missing-or-unsuitable-terminal-xterm-ghostty/
-      if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
-        export TERM=xterm-256color
-      fi
+        character = {
+          success_symbol = "[%%](bold #5fd787)";
+          error_symbol = "[%%](bold #ff5f5f)";
+          vicmd_symbol = "[V](bold #5fd787)";
+        };
 
-      setopt clobber
-      setopt extendedglob
-      setopt interactive_comments
-      setopt nobeep
-      setopt HIST_IGNORE_ALL_DUPS
-      setopt HIST_REDUCE_BLANKS
+        time = {
+          disabled = false;
+          format = "[$time]($style)";
+          style = "bold white";
+          use_12hr = false;
+        };
 
-      # NOTE: I had issues where fzf-tab wasn't including hidden files even
-      # though my FZF_DEFAULT_COMMAND specifies to do so.
-      # https://github.com/Aloxaf/fzf-tab/issues/193#issuecomment-784722265
-      setopt globdots
+        username = {
+          show_always = true;
+          format = "[$user]($style)";
+          style_user = "bold #ffa500";
+          style_root = "bold #ff5f5f";
+        };
 
-      eval "$(starship init zsh)"
-      eval "$(just --completions zsh)"
+        hostname = {
+          ssh_only = false;
+          disabled = false;
+          format = "[$hostname]($style)";
+          style = "bold white";
+        };
 
-      source ${pkgs.fzf}/share/fzf/completion.zsh
-      # NOTE: I had issues with zsh-vi-mode overwriting ^R
-      source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+        directory = {
+          read_only = ":ro";
+          format = "[$path](bold #ffa500)[$read_only](bold #ff5f5f)";
+        };
 
-      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-      source ${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh
+        git_status = {
+          ahead = "=>";
+          behind = "<=";
+          diverged = "<=>";
+          renamed = "r";
+          deleted = "x";
+        };
 
-      if [ -x "$(command -v stern)" ]; then
-        source <(stern --completion=zsh)
-      fi
+        git_branch = {
+          symbol = "";
+          format = "[$symbol$branch(:$remote_branch)]($style)";
+          style = "bold #5fd787";
+        };
 
-      if [ -x "$(command -v fastly)" ]; then
-        eval "$(fastly --completion-script-zsh)"
-      fi
+        git_commit = {
+          format = "[\\($hash$tag\\)]($style)";
+          style = "bold white";
+        };
 
-      if [ -x "$(command -v dagger)" ]; then
-        source <(dagger completion zsh)
-      fi
+        # kubernetes = {
+        #   symbol = "k8s:";
+        #   disabled = false;
+        #   format = "[$symbol$context( \($namespace\))]($style)";
+        # };
 
-      if [ -x "$(command -v k3d)" ]; then
-        source <(k3d completion zsh)
-      fi
-
-      eval $(keychain --eval --quiet ~/.ssh/config.d/work/fastly_rsa)
-
-      # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-completion.html#cli-command-completion-path
-      autoload bashcompinit && bashcompinit
-      autoload -Uz compinit && compinit
-
-      complete -C "${pkgs.awscli2}/bin/aws_completer" aws
-
-      eval "$(zoxide init zsh)"
-    '';
-  };
-
-  xdg.configFile."zsh/.zsh_history.default".source = ./zsh/.zsh_history.default;
-
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = true;
-      package.disabled = true;
-      format = "\\\[$username[@](bold #5fd787)$hostname\\\] \\\[$directory\\\] (\\\[($git_state )($git_status)($git_branch)($git_commit)\\\])($sudo)($golang)($helm)($lua)($nodejs)($ruby)($rust)($terraform)( \\\[$jobs\\\])( \\\[$status\\\])( \\\[$shell\\\])$line_break$time $character";
-
-      character = {
-        success_symbol = "[%%](bold #5fd787)";
-        error_symbol = "[%%](bold #ff5f5f)";
-        vicmd_symbol = "[V](bold #5fd787)";
-      };
-
-      time = {
-        disabled = false;
-        format = "[$time]($style)";
-        style = "bold white";
-        use_12hr = false;
-      };
-
-      username = {
-        show_always = true;
-        format = "[$user]($style)";
-        style_user = "bold #ffa500";
-        style_root = "bold #ff5f5f";
-      };
-
-      hostname = {
-        ssh_only = false;
-        disabled = false;
-        format = "[$hostname]($style)";
-        style = "bold white";
-      };
-
-      directory = {
-        read_only = ":ro";
-        format = "[$path](bold #ffa500)[$read_only](bold #ff5f5f)";
-      };
-
-      git_status = {
-        ahead = "=>";
-        behind = "<=";
-        diverged = "<=>";
-        renamed = "r";
-        deleted = "x";
-      };
-
-      git_branch = {
-        symbol = "";
-        format = "[$symbol$branch(:$remote_branch)]($style)";
-        style = "bold #5fd787";
-      };
-
-      git_commit = {
-        format = "[\\($hash$tag\\)]($style)";
-        style = "bold white";
-      };
-
-      # kubernetes = {
-      #   symbol = "k8s:";
-      #   disabled = false;
-      #   format = "[$symbol$context( \($namespace\))]($style)";
-      # };
-
-      aws = {
-        symbol = "aws:";
-        format = "[($profile)(\\\($region\\\))]($style)";
-        style = "bold #ffe500";
-      };
-      gcloud = {
-        symbol = "gcp:";
-        format = "[(\\\($project\\\))]($style)";
-        style = "bold #2950c5";
-      };
-      golang = {
-        symbol = "go:";
-        format = "[$symbol($version)]($style)";
-      };
-      lua = {
-        symbol = "lua:";
-        format = "[$symbol($version)]($style)";
-      };
-      nix_shell = {
-        symbol = "nix-shell";
-        format = "[$symbol]($style)";
-      };
-      python = {
-        symbol = "py:";
-        format = "[$symbol($version)]($style)";
-      };
-      ruby = {
-        symbol = "rb:";
-        format = "[$symbol($version)]($style)";
-      };
-      rust = {
-        symbol = "rs:";
-        style = "bold #cf4910";
-        format = "[$symbol($version)]($style)";
-      };
-      terraform = {
-        symbol = "tf:";
-        format = "[$symbol($version)]($style)";
-      };
-      sudo.symbol = "sudo:";
-      cmd_duration = {
-        min_time = 2000;
-        format = "took [$duration]($style)";
-        style = "bold yellow";
-      };
-      jobs = {
-        symbol = "!!!";
-        format = "[$symbol( $number)]($style)";
+        aws = {
+          symbol = "aws:";
+          format = "[($profile)(\\\($region\\\))]($style)";
+          style = "bold #ffe500";
+        };
+        gcloud = {
+          symbol = "gcp:";
+          format = "[(\\\($project\\\))]($style)";
+          style = "bold #2950c5";
+        };
+        golang = {
+          symbol = "go:";
+          format = "[$symbol($version)]($style)";
+        };
+        lua = {
+          symbol = "lua:";
+          format = "[$symbol($version)]($style)";
+        };
+        nix_shell = {
+          symbol = "nix-shell";
+          format = "[$symbol]($style)";
+        };
+        python = {
+          symbol = "py:";
+          format = "[$symbol($version)]($style)";
+        };
+        ruby = {
+          symbol = "rb:";
+          format = "[$symbol($version)]($style)";
+        };
+        rust = {
+          symbol = "rs:";
+          style = "bold #cf4910";
+          format = "[$symbol($version)]($style)";
+        };
+        terraform = {
+          symbol = "tf:";
+          format = "[$symbol($version)]($style)";
+        };
+        sudo.symbol = "sudo:";
+        cmd_duration = {
+          min_time = 2000;
+          format = "took [$duration]($style)";
+          style = "bold yellow";
+        };
+        jobs = {
+          symbol = "!!!";
+          format = "[$symbol( $number)]($style)";
+        };
       };
     };
-  };
-}
+  }
