383f0bbfb4f3e4d4808be247f8eef6ed1a497e90
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-01-10 09:28:04 -0500

    address git warnings
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/git.nix
+++ home-modules/shared/git.nix
@@ -1,58 +1,70 @@
 { config, ... }: {
-  programs.git = {
-    enable = true;
-    userName = "Colton J. McCurdy";
-    userEmail = "mccurdyc22@gmail.com";
-    signing = {
-      key = "9CD9D30EBB7B1EEC";
-      signByDefault = true;
-    };
-    extraConfig = {
-      help = { autoCorrect = 5; }; # Run guessed command after 500ms if there is only one reasonable guess
-      init = { defaultBranch = "main"; };
-      core = {
-        excludesfile = "${config.home.homeDirectory}/.config/git/ignore";
-      };
-      url = {
-        # https://gist.github.com/StevenACoffman/866b06ed943394fbacb60a45db5982f2#how-to-go-get-private-repos-using-ssh-key-auth-instead-of-password-auth
-        "git@github.com:" = {
-          insteadOf = "https://github.com/";
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Colton J. McCurdy";
+          email = "mccurdyc22@gmail.com";
+
+          alias = {
+            l = "log --graph --topo-order --abbrev-commit --date=short --decorate --all --boundary";
+            lb = "!f() { git log --graph $(git branch --show-current) --not origin/main --decorate --oneline; }; f";
+            ll = "log --graph --all --decorate --oneline";
+            ls = "log --pretty=format:\"%C(green)%h %C(yellow)[%ad]%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=relative";
+            ld = "log --all --graph --abbrev-commit --decorate --pretty=format:\"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset) %C(dim white)- %an%C(reset)\"";
+            cleanbranches = "!f() { git branch | grep -v 'main' | xargs git branch -D; }; f";
+            m = "mergetool";
+            review = "!nvim -c DiffReview";
+            reviews = "!nvim -c DiffReviewStaged";
+          };
+        };
+        help = { autoCorrect = 5; }; # Run guessed command after 500ms if there is only one reasonable guess
+        init = { defaultBranch = "main"; };
+        core = {
+          excludesfile = "${config.home.homeDirectory}/.config/git/ignore";
+        };
+        url = {
+          # https://gist.github.com/StevenACoffman/866b06ed943394fbacb60a45db5982f2#how-to-go-get-private-repos-using-ssh-key-auth-instead-of-password-auth
+          "git@github.com:" = {
+            insteadOf = "https://github.com/";
+          };
+        };
+        mergetool = {
+          prompt = false;
+          keepBackup = false;
+        };
+        merge = {
+          tool = "nvimmerge";
+          conflictStyle = "diff3";
+        };
+        mergetool."nvimmerge" = {
+          # http://vimcasts.org/episodes/fugitive-vim-resolving-merge-conflicts-with-vimdiff/
+          name = "nvimmerge";
+          trustExitCode = true;
+          cmd = "nvim -f -c Gdiffsplit! $MERGED";
+        };
+        diff = {
+          tool = "nvimdiff";
+        };
+        difftool."nvimdiff" = {
+          cmd = "nvim -d $LOCAL $REMOTE";
+          # Be able to abort all diffs with `:cq` or `:cquit`
+          trustExitCode = true;
         };
       };
-      mergetool = {
-        prompt = false;
-        keepBackup = false;
+      signing = {
+        key = "9CD9D30EBB7B1EEC";
+        signByDefault = true;
       };
-      merge = {
-        tool = "nvimmerge";
-        conflictStyle = "diff3";
-      };
-      mergetool."nvimmerge" = {
-        # http://vimcasts.org/episodes/fugitive-vim-resolving-merge-conflicts-with-vimdiff/
-        name = "nvimmerge";
-        trustExitCode = true;
-        cmd = "nvim -f -c Gdiffsplit! $MERGED";
-      };
-      diff = {
-        tool = "nvimdiff";
-      };
-      difftool."nvimdiff" = {
-        cmd = "nvim -d $LOCAL $REMOTE";
-        # Be able to abort all diffs with `:cq` or `:cquit`
-        trustExitCode = true;
-      };
+      ignores = [
+        ".devenv"
+        ".direnv"
+        "build_"
+        ".aider*"
+      ];
     };
-    aliases = {
-      l = "log --graph --topo-order --abbrev-commit --date=short --decorate --all --boundary";
-      lb = "!f() { git log --graph $(git branch --show-current) --not origin/main --decorate --oneline; }; f";
-      ll = "log --graph --all --decorate --oneline";
-      ls = "log --pretty=format:\"%C(green)%h %C(yellow)[%ad]%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=relative";
-      ld = "log --all --graph --abbrev-commit --decorate --pretty=format:\"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset) %C(dim white)- %an%C(reset)\"";
-      cleanbranches = "!f() { git branch | grep -v 'main' | xargs git branch -D; }; f";
-      m = "mergetool";
-      review = "!nvim -c DiffReview";
-      reviews = "!nvim -c DiffReviewStaged";
-    };
+
     delta = {
       enable = true;
       options = {
@@ -74,11 +86,5 @@
         minus-style = ''red bold "#ffcccc"'';
       };
     };
-    ignores = [
-      ".devenv"
-      ".direnv"
-      "build_"
-      ".aider*"
-    ];
   };
 }
