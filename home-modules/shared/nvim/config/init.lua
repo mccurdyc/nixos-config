dfa2baeaaf16c3e3526f2b038e4be7a7cdbeb156
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-02-18 09:29:29 -0500

    [nvim] macos now warns when pasting in nvim within ssh
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/shared/nvim/config/init.lua
+++ home-modules/shared/nvim/config/init.lua
@@ -88,7 +88,7 @@ local buffer = { o, bo }
 local window = { o, wo }
 
 opt("title", false)
-opt("clipboard", "unnamedplus")
+-- opt("clipboard", "unnamedplus") -- macos prints an annoying permission message every time I paste in NeoVim when ssh'd to a machine.
 opt("swapfile", false, buffer)
 opt("wrap", false, window)
 opt("linebreak", true, window)
