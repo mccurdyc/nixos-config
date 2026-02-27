75ab4c05e744c73517695dd926ed5cb2185f05a0
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-02-27 11:16:58 -0500

    home-modules cleanup
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- home-modules/work/default.nix
+++ home-modules/work/default.nix
@@ -1,23 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  home.packages = with pkgs; [
-    cntr
-    infra
+  imports = [
+    ./packages.nix
+    ./zsh.nix
   ];
-
-  programs.zsh.initExtra = ''
-    opencode() {
-      aws sts get-caller-identity --profile bedrock
-      if [ $? -eq 0 ]; then
-          echo "SSO session is valid"
-      else
-          echo "refreshing SSO session"
-          aws --no-browser --use-device-code sso login --profile bedrock
-      fi
-      AWS_PROFILE=bedrock \
-      AWS_REGION=us-east-2 \
-      command opencode "$@"
-    }
-  '';
 }
