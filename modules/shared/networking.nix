d777c38e6089e665ea16d8eab94529b0bab2a97b
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-02-27 10:15:01 -0500

    removes systemd config from shared
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- modules/shared/networking.nix
+++ modules/shared/networking.nix
@@ -5,9 +5,4 @@
     enable = true;
     package = pkgs.tailscale;
   };
-
-  # Protect tailscaled from the OOM killer.
-  systemd.services.tailscaled.serviceConfig = {
-    OOMScoreAdjust = -900;
-  };
 }
