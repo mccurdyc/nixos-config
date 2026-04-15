741cf8deca3f1c148d7f2acfcd4c987124de5eba
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-04-15 14:38:10 -0400

    remove DOCKER_DEFAULT_PLATFORM env var

--- home-modules/shared/environment.nix
+++ home-modules/shared/environment.nix
@@ -6,7 +6,6 @@
     VISUAL = "${pkgs.neovim}/bin/nvim";
 
     BROWSER = "firefox";
-    DOCKER_DEFAULT_PLATFORM = "linux/amd64";
 
     FZF_CTRL_T_COMMAND = "fd --type f --hidden --no-ignore --exclude vendor --exclude .git";
     FZF_ALT_C_OPTS = "--preview 'tree -C {} | head -200'";
